d96a29d3440e4bcfd63a876f9d2019fca5df5483
stan1ee0 <stan1ee@naver.com>
2023-06-29 11:37:25 +0900

    Initial Server Code Setup

---
+++ server/src/main/java/org/vision0/vision0verflow/Vision0verflowApplication.java
@@ -0,0 +1,13 @@
+package org.vision0.vision0verflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vision0verflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vision0verflowApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/vision0/vision0verflow/Vision0verflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vision0.vision0verflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vision0verflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
