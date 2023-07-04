840423d29eb7be67833bc6c5b881a3edff99e9ee
stan1ee0 <stan1ee@naver.com>
2023-07-04 20:47:30 +0900

    [stan1ee0] Initial Ask Page Implementation - Implemented AskPage - Implemented QuestionInput

---
+++ client/src/components/QuestionInput.js
@@ -0,0 +1,93 @@
+import { styled } from 'styled-components';
+
+const Main = styled.div`
+  margin-bottom: 48px;
+  display: block;
+`;
+
+const Box = styled.div`
+  background-color: white;
+  border-color: hsl(210, 8%, 90%);
+  border-radius: 3px;
+  border-style: solid;
+  border-width: 1px;
+  padding: 24px;
+  margin: 20px 0;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: 1px solid hsl(210, 8%, 75%);
+  border-radius: 3px;
+  cursor: unset;
+  font-size: 13px;
+  opacity: unset;
+  padding: 0.6em 0.7em 0.6em 0.7em;
+`;
+
+const Textarea = styled.textarea`
+  width: 100%;
+`;
+
+const Button = styled.button`
+  margin-top: 8px;
+  background-color: hsl(206, 100%, 52%);
+  border: 1px solid transparent;
+  border-radius: 3px;
+  box-shadow: inset 0 1px 0 0 hsla(0, 0%, 100%, 0.4);
+  font-size: 13px;
+  padding: 0.8em;
+  cursor: pointer;
+  display: inline-block;
+  font-weight: normal;
+  line-height: calc(15 / 13);
+  position: relative;
+  outline: none;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+`;
+
+export default function QuestionInput() {
+  return (
+    <form action="/questions/ask/submit" method="post">
+      <Main>
+        <Box>
+          <div>
+            <label htmlFor="title"> Title </label>
+          </div>
+          <div>
+            <label htmlFor="title">
+              {' '}
+              Be specific and imagine you’re asking a question to another
+              person.{' '}
+            </label>
+          </div>
+          <div>
+            <Input
+              id="title"
+              name="title"
+              type="text"
+              maxLength={300}
+              placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
+            />
+          </div>
+        </Box>
+        <Box>
+          <div>
+            <label htmlFor="body"> Body </label>
+          </div>
+          <div>
+            <label htmlFor="body">
+              Introduce the problem and expand on what you put in the title.
+            </label>
+          </div>
+          <div>
+            <Textarea rows={10}></Textarea>
+          </div>
+        </Box>
+        <Button type="button">Post Your Question</Button>
+      </Main>
+    </form>
+  );
+}
---
+++ client/src/pages/AskPage.js
@@ -0,0 +1,86 @@
+import { styled } from 'styled-components';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import QuestionInput from '../components/QuestionInput';
+
+const Container = styled.div`
+  max-width: 100%;
+  justify-content: center;
+  margin: 0;
+  width: 100%;
+  background: none;
+  display: flex;
+  position: relative;
+  flex: 1 0 auto;
+  text-align: left;
+  background-color: hsl(210, 8%, 97.5%);
+`;
+
+const Content = styled.div`
+  min-height: 750px;
+  overflow: visible;
+  width: 100%;
+  max-width: 1264px;
+  margin: 0;
+  background-color: transparent;
+  border-left: 0;
+  border-right: 0;
+  border-radius: 0;
+  border: 1px solid var(--theme-content-border-color);
+  border-top-width: 0;
+  border-bottom-width: 0;
+  padding: var(--su24);
+  box-sizing: border-box;
+`;
+
+const Notice = styled.div`
+  padding: 24px;
+  background-color: hsl(206, 100%, 97%);
+  border-style: solid;
+  border-radius: 3px;
+  border-width: 1px;
+`;
+
+export default function AskPage() {
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Content>
+          <div>
+            <div>
+              <h1>Ask a public question</h1>
+            </div>
+            <Notice>
+              <h2>Writing a good question</h2>
+              <p>
+                You’re ready to ask a programming-related question and this form
+                will help guide you through the process.
+              </p>
+              <p>
+                Looking to ask a non-programming question? See the topics here
+                to find a relevant site.
+              </p>
+              <h5>Steps</h5>
+              <ul>
+                <li>Summarize your problem in a one-line title.</li>
+                <li>Describe your problem in more detail.</li>
+                <li>
+                  Describe what you tried and what you expected to happen.
+                </li>
+                <li>
+                  Add “tags” which help surface your question to members of the
+                  community.
+                </li>
+                <li>Review your question and post it to the site.</li>
+              </ul>
+            </Notice>
+          </div>
+          <QuestionInput />
+        </Content>
+      </Container>
+      <Footer />
+    </div>
+  );
+}
