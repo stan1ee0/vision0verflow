95bc23aa65955a52cc6717bc5be120d37844de98
stan1ee0 <stan1ee@naver.com>
2023-07-11 16:13:14 +0900

    [stan1ee0] Additional Header Implementation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -75,6 +75,25 @@ const NavLi = styled.li`
   display: inline-flex;
 `;
 
+const NavA = styled.a`
+  padding-left: 12px !important;
+  padding-right: 12px !important;
+  margin: 0 !important;
+`;
+
+const AvatarContainer = styled.div`
+  padding: 0;
+  overflow: hidden;
+`;
+
+const NavSvg = styled.svg`
+  vertical-align: text-top;
+`;
+
+const AchievA = styled.a`
+  color = hsl(140,40%,40%) !important;
+`;
+
 const LoginA = styled.a`
   background-color: hsl(205, 46%, 92%);
   color: hsl(205, 47%, 42%);
@@ -99,6 +118,8 @@ const SignupA = styled.a`
 export default function Header() {
   const [keyword, setKeyword] = useState('');
 
+  const isLoggedIn = !!localStorage.getItem('token');
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
@@ -106,17 +127,25 @@ export default function Header() {
   return (
     <HeaderContainer>
       <InnerContainer>
+      {isLoggedIn ? null : (
         <a className='header-button' href="/users/login">
           <img src={hamburger} alt="Hamburger"/>
         </a>
+      )}
         <a className='header-logo' href="/">
           <LogoSpan>Stack Overflow</LogoSpan>
         </a>
-        <ol className='navigation'>
-          <li><A className='navigation-item'>About</A></li>
-          <li><A className='navigation-item'>Products</A></li>
-          <li><A className='navigation-item'>For Teams</A></li>
-        </ol>
+        {isLoggedIn ? (
+          <ol className='navigation'>
+            <li><A className='navigation-item'>Products</A></li>
+          </ol>
+        ) : (
+          <ol className='navigation'>
+            <li><A className='navigation-item'>About</A></li>
+            <li><A className='navigation-item'>Products</A></li>
+            <li><A className='navigation-item'>For Teams</A></li>
+          </ol>          
+        )}
         <Form onSubmit={handleSubmit}>
           <InputContainer>
             <Input className="input" name="q" type="text" role="combobox" autoComplete="off"
@@ -129,10 +158,52 @@ export default function Header() {
           </InputContainer>
         </Form>
         <Nav>
+        {isLoggedIn ? (
+          <ol className='header-content'>
+            <NavLi role="none">
+              <NavA className="header-item user-card" href="/users/1">
+                <AvatarContainer className='header-avatar'>
+                  <img className='header-avatar-image' alt='Vision0'
+                    src='https://s3.amazonaws.com/comicgeeks/characters/avatars/1616.jpg?t=1687973152'
+                  />
+                </AvatarContainer>       
+              </NavA>
+            </NavLi>
+            <NavLi role="none">
+              <A className="header-item">
+                <NavSvg width="20" height="18" viewBox="0 0 20 18">
+                  <path d="M4.63 1h10.56a2 2 0 0 1 1.94 1.35L20 10.79V15a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-4.21l2.78-8.44c.25-.8 1-1.36 1.85-1.35Zm8.28 12 2-2h2.95l-2.44-7.32a1 1 0 0 0-.95-.68H5.35a1 1 0 0 0-.95.68L1.96 11h2.95l2 2h6Z"></path>
+                </NavSvg>
+              </A>
+            </NavLi>
+            <NavLi role="none">
+              <AchievA className="header-item">
+              <NavSvg width="18" height="18" viewBox="0 0 18 18">
+                <path d="M15 2V1H3v1H0v4c0 1.6 1.4 3 3 3v1c.4 1.5 3 2.6 5 3v2H5s-1 1.5-1 2h10c0-.4-1-2-1-2h-3v-2c2-.4 4.6-1.5 5-3V9c1.6-.2 3-1.4 3-3V2h-3ZM3 7c-.5 0-1-.5-1-1V4h1v3Zm8.4 2.5L9 8 6.6 9.4l1-2.7L5 5h3l1-2.7L10 5h2.8l-2.3 1.8 1 2.7h-.1ZM16 6c0 .5-.5 1-1 1V4h1v2Z"></path>
+              </NavSvg>
+              </AchievA>
+            </NavLi>
+            <NavLi role="none">
+              <NavA className="header-item">
+                <NavSvg width="18" height="18" viewBox="0 0 18 18">
+                  <path d="M9 1C4.64 1 1 4.64 1 9c0 4.36 3.64 8 8 8 4.36 0 8-3.64 8-8 0-4.36-3.64-8-8-8Zm.81 12.13c-.02.71-.55 1.15-1.24 1.13-.66-.02-1.17-.49-1.15-1.2.02-.72.56-1.18 1.22-1.16.7.03 1.2.51 1.17 1.23ZM11.77 8c-.59.66-1.78 1.09-2.05 1.97a4 4 0 0 0-.09.75c0 .05-.03.16-.18.16H7.88c-.16 0-.18-.1-.18-.15.06-1.35.66-2.2 1.83-2.88.39-.29.7-.75.7-1.24.01-1.24-1.64-1.82-2.35-.72-.21.33-.18.73-.18 1.1H5.75c0-1.97 1.03-3.26 3.03-3.26 1.75 0 3.47.87 3.47 2.83 0 .57-.2 1.05-.48 1.44Z"></path>
+                </NavSvg>
+              </NavA>
+            </NavLi>
+            <NavLi role="none">
+              <NavA className="header-item" href='/users/logout'>
+                <NavSvg width="18" height="18" viewBox="0 0 18 18">
+                  <path d="M15 1H3a2 2 0 0 0-2 2v2h16V3a2 2 0 0 0-2-2ZM1 13c0 1.1.9 2 2 2h8v3l3-3h1a2 2 0 0 0 2-2v-2H1v2Zm16-7H1v4h16V6Z"></path>
+                </NavSvg>
+              </NavA>
+            </NavLi>
+          </ol>
+        ) : (
           <ol className='header-content'>
             <NavLi role="none"><LoginA className="button" href="/users/login">Log in</LoginA></NavLi>
             <NavLi role="none"><SignupA className="button">Sign up</SignupA></NavLi>
           </ol>
+        )}
         </Nav>
       </InnerContainer>
     </HeaderContainer>
